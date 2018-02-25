8266c7db447a36055ebcf55cb39d16b6787c25e1
Ermin Avdagic <eavdagic@gmail.com>
2018-02-24 20:15:14 -0500

    changed docker builder to use Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM golang
+
+RUN go get -v github.com/local-magic-leagues/mtg-golang-league 
--- docker-builder.sh
+++ docker-builder.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-set -e
 
-IMAGE_NAME="golang"
+IMAGE_NAME="mtg"
 
-# download and create a golang docker container and gets the mtg-golang-league repo
-echo "  Creating a golang container..."
-docker run $IMAGE_NAME go get -v github.com/local-magic-leagues/mtg-golang-league
+echo "  Building from Dockerfile..."
+docker build -t $IMAGE_NAME .
+echo "  Done"
 
-# creates a new docker image called mtg-golang-league and commits it
-echo "  Committing mtg-golang-container..."
-docker commit $(docker ps -lq) mtg-golang-league
+echo "  Containerizing mtg image"
+docker run -d -p 8000:8000 $IMAGE_NAME mtg-golang-league
+echo "****  Done. Listening on port 8000    ****"
 
-# runs the mtg-golang-league image that listens on port 8000
-echo "  Running mtg-golang-container..."
-docker run -d -p 8000:8000 mtg-golang-league mtg-golang-league
-echo "mtg-golang-league container is running and listening on port 8000..."
